7209d6ca7f07bd8f25746a317933b18b842b2c30
Agus Suaratwan <kadeksuwar@gmail>
2021-07-27 13:20:10 +0800

    perbaikan pada menu import

--- database/migrations/2021_07_22_105039_create_product_transaction_table.php
+++ database/migrations/2021_07_22_105039_create_product_transaction_table.php
@@ -16,9 +16,9 @@ class CreateProductTransactionTable extends Migration
         Schema::create('product_transaction', function (Blueprint $table) {
             $table->foreignId('product_id')->constrained();
             $table->foreignId('transaction_id')->constrained();
-            $table->integer('qty')->default(0);
-            $table->double('price')->default(0);
-            $table->double('subtotal')->default(0);
+            $table->integer('qty')->nullable();
+            $table->double('price')->nullable();
+            $table->double('subtotal')->nullable();
         });
     }
 
